1711009f574c97913004643870887ba697ff1450
camilaEsc4 <cescobard@estudiante.uniajc.edu.co>
2025-11-18 15:25:44 -0500

    modificacion

--- src/App.java
+++ src/App.java
@@ -6,8 +6,8 @@ import Controler.InventarioControler;
 public class App {
 
     public static void main(String[] args) {
-        BasedeDatos baseDatos = new BasedeDatos();
-        InventarioVista vista = new InventarioVista();
+    BasedeDatos baseDatos = new BasedeDatos();
+    InventarioVista vista = new InventarioVista();
         InventarioControler controlador = new InventarioControler(baseDatos, vista);
         controlador.iniciar();
     }
